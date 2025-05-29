9cb4c9de0cff55ddba52f93a791018448af2dbe4
fenyadria <fenyadriam@gmail.com>
2025-05-29 20:30:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from tkinter import Tk
-from app import AplikasiTkinter
+from tkinter import Tk, Frame
+from slide1 import Slide1
+from slide2 import Slide2
+
+class AplikasiTkinter:
+  def __init__(self, root):
+    self.root = root
+    self.root.title("Kelompok 9")
+    self.root.geometry("600x400")
+    self.root.resizable(True, True)
+
+    self.container = Frame(self.root)
+    self.container.pack(expand=True, fill="both")
+    self.slides = {}
 
 if __name__ == "__main__":
   root = Tk()
