68a3e48f828d3d32431def22dd64b6e64b71ee4d
SakinahInaa <sakinahismail15@gmail.com>
2025-05-30 00:20:19 +0700

    Update slide1.py

--- slide1.py
+++ slide1.py
@@ -8,3 +8,10 @@ class Slide1(Frame):
         self.canvas.pack(fill="both", expand=True)
 
         self.canvas.bind("<Configure>", self.resize_background)
+
+        self.tombol_lanjut = Button(self, text="Perkenalan Kelompok",
+                                    command=lambda: controller.show_slide("Slide2"),
+                                    font=("Algerian", 15, "bold"),
+                                    bg="#5B2C2C", fg="white",
+                                    activebackground="#0059b3", activeforeground="white",
+                                    padx=20, pady=10, borderwidth=0, relief="flat")
