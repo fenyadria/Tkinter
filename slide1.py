8f1a1472101993dd344678a6b22116b908dae5d3
SakinahInaa <sakinahismail15@gmail.com>
2025-05-30 00:19:30 +0700

    Update slide1.py

--- slide1.py
+++ slide1.py
@@ -1,1 +1,10 @@
+class Slide1(Frame):
+    def __init__(self, parent, controller):
+        super().__init__(parent)
+        self.controller = controller
 
+        self.original_image = Image.open("Tyler The Creator.jpg")
+        self.canvas = Canvas(self, width=1300, height=650, highlightthickness=0)
+        self.canvas.pack(fill="both", expand=True)
+
+        self.canvas.bind("<Configure>", self.resize_background)
