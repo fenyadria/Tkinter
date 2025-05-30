ed53bf1e1fc1c1ced7f425fb38ae52b00ec77876
kath <kamilaputrihasan@gmail.com>
2025-05-30 20:34:19 +0700

    Update slide2.py

--- slide2.py
+++ slide2.py
@@ -4,3 +4,10 @@ class Slide2(Frame):
     
         self.controller = controller
         self.configure(bg="white")
+        
+        title = Label(self, text="DISUSUN OLEH", font=("Segoa UI", 50, "bold"), fg="black", bg="white")
+        title.pack(pady=20)
+        
+        team_frame = Frame(self, bg="white")
+        team_frame.pack(pady=20)
+
