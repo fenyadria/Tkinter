826008e6b3a4bad1a65f9892e6e03519fd930f6e
juken8000 <arkhanalhakim71@gmail.com>
2025-05-30 22:23:39 +0700

    Update slide1.py

--- slide1.py
+++ slide1.py
@@ -35,3 +35,69 @@ class Slide1(Frame):
         self.floating_text2 = None
         self.float_offset = 0
         self.float_direction = 1
+
+def resize_background(self, event):
+        resized = self.original_image.resize((event.width, event.height), Image.Resampling.LANCZOS)
+        self.bg = ImageTk.PhotoImage(resized)
+
+        self.canvas.delete("all")
+        self.canvas.create_image(0, 0, image=self.bg, anchor="nw")
+
+        self.center_x = event.width // 2
+        self.center_y = event.height // 3
+        if not self.floating_text1 and not self.floating_text2:
+            self.canvas.delete("all")
+            self.canvas.create_image(0, 0, image=self.bg, anchor="nw")
+
+            self.floating_text1_outline_ids, self.floating_text1 = create_text_with_outline(
+            self.canvas, self.center_x, self.center_y - 60,
+            "WELCOME",
+            ("Old English Text MT", 120, "bold"),
+            fill="#5B2C2C",
+            outline_color="white",
+            outline_width=2
+    )
+
+            self.floating_text2_outline_ids, self.floating_text2 = create_text_with_outline(
+            self.canvas, self.center_x, self.center_y + 60,
+            "Project Kelompok 9",
+            ("Magneto", 50, "bold"),
+            fill="#5B2C2C",
+            outline_color="white",
+            outline_width=2
+    )
+
+            self.tombol_lanjut_id = self.canvas.create_window(
+                self.center_x, self.center_y + 100,  # Tetap
+                window=self.tombol_lanjut
+            )
+            self.tombol_ke_slide3_id = self.canvas.create_window(
+                self.center_x, self.center_y + 160,
+                window=self.tombol_ke_slide3
+            )
+
+            self.animate_floating_text()
+        else:
+            self.canvas.coords(self.floating_text1, self.center_x, self.center_y - 60 + self.float_offset)
+            self.canvas.coords(self.floating_text2, self.center_x, self.center_y + 60 + self.float_offset)
+            self.canvas.coords(self.tombol_lanjut_id, self.center_x, self.center_y + 100)
+            self.canvas.coords(self.tombol_ke_slide3_id, self.center_x, self.center_y + 160)
+
+    def animate_floating_text(self):
+        self.float_offset += self.float_direction
+        if abs(self.float_offset) > 20:
+          self.float_direction *= -1
+
+        ox = [ -2, -2, -2, 0, 0, 2, 2, 2 ]
+        oy = [ -2, 0, 2, -2, 2, -2, 0, 2 ]
+        for i, outline_id in enumerate(self.floating_text1_outline_ids):
+          self.canvas.coords(outline_id, self.center_x + ox[i], self.center_y - 60 + oy[i] + self.float_offset)
+        self.canvas.coords(self.floating_text1, self.center_x, self.center_y - 60 + self.float_offset)
+        for i, outline_id in enumerate(self.floating_text2_outline_ids):
+          self.canvas.coords(outline_id, self.center_x + ox[i], self.center_y + 20 + oy[i] + self.float_offset)
+        self.canvas.coords(self.floating_text2, self.center_x, self.center_y + 20 + self.float_offset)
+
+        self.after(50, self.animate_floating_text)
+
+          
+
