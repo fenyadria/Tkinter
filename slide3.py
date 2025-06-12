6b8c725c438c304437eddf4a2e18d62c4aeec448
fenyadria <fenyadriam@gmail.com>
2025-06-12 13:00:04 +0700

    Update slide3.py

--- slide3.py
+++ slide3.py
@@ -4,10 +4,21 @@ from PIL import Image, ImageTk
 class Slide3(Frame):
   def __init__(self, parent, controller):
     super().__init__(parent)
+    
     self.controller = controller
     self.configure(bg="white")
 
-    
+    subframe3 = Frame(self, background="black", height="80")
+    subframe3.pack(fill="x", side="top") 
+
+    subframe4 = Frame(self, height="70", bg=self['bg'])
+    subframe4.pack(fill="x", side="bottom")
+
+    subframe = Frame(self, background="blue")
+    subframe.pack(expand = True, fill = "both", side="left")
+ 
+    subframe2 = Frame(self, background="red")
+    subframe2.pack(expand=True, fill="both", side="left")
     
     btn_kembali = Button(subframe3, text="Kembali",
                          command=lambda: controller.show_slide("Slide1"),
