3ab728448fe9eb1cd2c6d59204603c293edb4b5f
kath <kamilaputrihasan@gmail.com>
2025-06-12 12:56:01 +0700

    Update slide3.py

--- slide3.py
+++ slide3.py
@@ -1,1 +1,15 @@
+class Slide3(Frame):
+  def __init__(self, parent, controller):
+    super().__init__(parent)
+    self.controller = controller
+    self.configure(bg="white")
+    
+    btn_kembali = Button(subframe3, text="Kembali",
+                         command=lambda: controller.show_slide("Slide1"),
+                         font=("Algerian", 15, "bold"),
+                         bg="#5B2C2C", fg="white",
+                         activebackground="#0059b3", activeforeground="white"
+                         padx=10, pady=10, borderwidth=0, relief="flat")
+    btn_kembali.pack(side="right", pady = 10)
+
 
